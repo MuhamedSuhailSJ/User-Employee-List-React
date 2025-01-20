30d1670e6b4e05c0708bfe734969cfc23d0adf8e
MuhamedSuhailSJ <s.j.muhamedsuhail@outlook.in>
2025-01-20 19:14:34 +0530

    phase 3

--- src/components/UserDetail/styledComponents.js
+++ src/components/UserDetail/styledComponents.js
@@ -28,12 +28,13 @@ export const UserDetailCard = styled.div`
   border-radius: 4px;
   overflow: scroll;
   width: 80%;
-  height: 90%;
+  height: 90vh;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
     rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
     rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
   @media (max-width: 768px) {
-    width:350px;
+    width:95%;
+    height:80vh;
   }
 `;
 
